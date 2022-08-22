816888297506e04ce64168447ebfea4cd236986a
Pamela Albuquerque <artemis121289@gmail.com>
2022-08-22 17:58:17 -0300

    estrutura

---
+++ src/main/java/com/carteiradevacinacao/CarteiraDeVacinacaoApplication.java
@@ -0,0 +1,13 @@
+package com.carteiradevacinacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraDeVacinacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraDeVacinacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carteiradevacinacao/CarteiraDeVacinacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carteiradevacinacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraDeVacinacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
