b8f49055fbd35bf7c6385e300c25b0e35b14f3a4
Pâmela Albuquerque <80077413+Pamt-o@users.noreply.github.com>
2022-10-01 13:17:03 -0300

    Update HomeController.java

--- src/main/java/com/carteiradevacinacao/controllers/HomeController.java
+++ src/main/java/com/carteiradevacinacao/controllers/HomeController.java
@@ -8,15 +8,9 @@ import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.carteiradevacinacao.Servico.CookieService;
 
 @Controller
 public class HomeController {
-  @GetMapping("/")
-  public String index(Model model, HttpServletRequest request) throws UnsupportedEncodingException{
-    model.addAttribute("nome", CookieService.getCookie(request, "nomeUsuario"));
-    return "home/index";
-    }
   
    @GetMapping("/")
     public String index(Model model){
